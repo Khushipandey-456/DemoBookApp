974aa4b5e591b908e2df3c30a964c50c742f1e0d
Khushipandey-456 <kp9761560@gmail.com>
2024-12-28 15:12:18 +0530

    LoginCompleted

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,17 @@ form.addEventListener("submit",function(){
     event.preventDefault()
     let email = form.email.vaule;
     let pass = form.password.value;
-    if(email==="admin@empher.com" && pass ==="empher@123"){
+//    console.log(email,pass)
+    if(email=="admin@empher.com" && pass =="empher@123"){
         alert("logged in Successfully.....!")
     window.location.href="admin.html"
-}
-    else{
-        if(email==="user@empher.com" && pass ==="user@123"){
+      }
+    else if(email==="user@empher.com" && pass ==="user@123"){
             alert("logged in Successfully.....!")
         window.location.href="books.html"
 
+    }else{
+        alert("something went wrong")
     }
-}
+
 })
